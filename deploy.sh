257b4c4852b87e03bf565cd5de6f49b5feac169e
Jaehan Lee <leejh17025@gmail.com>
2022-10-14 17:35:57 +0900

    Delete .vscode

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,10 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-git push -f git@github.com:goor33n/goor33n.github.io.git main
+# git push -f git@github.com:goor33n/goor33n.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:goor33n/goor33n-portfolio-svelte.git main:gh-pages
 
 cd -
